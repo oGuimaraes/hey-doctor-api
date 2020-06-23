ac32dbd5ddea1b49ba7916f41d19f31281f3f37c
oGuimaraes <otaviovgsr@gmail.com>
2020-06-22 23:38:29 -0300

    feat: User model

--- hey-doctor/src/routes.js
+++ hey-doctor/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Otávio Guimarães',
+    email: 'otaviovgsrs@gmail.com',
+    password_hash: '1283712893',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
