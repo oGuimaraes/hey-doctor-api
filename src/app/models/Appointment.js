c7c03016842e3066bfa2766a33cbfecd2ccaeefb
oGuimaraes <otaviovgsr@gmail.com>
2020-06-23 23:45:29 -0300

    feat: create migration and model of appointments

---
+++ src/app/models/Appointment.js
@@ -0,0 +1,25 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Appointment extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    /* In the case of more than one relationship, the use of 'as' is mandatory */
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
+  }
+}
+
+export default File;
