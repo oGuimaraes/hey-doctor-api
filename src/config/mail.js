90e5a4136d2cc04cc2df379d0418929ef00edf2b
oGuimaraes <otaviovgsr@gmail.com>
2020-06-24 18:39:21 -0300

    feat: configure nodemailer and send email in cancelling appointment

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'c098a602de54ed',
+    pass: 'c42ff67bce7237',
+  },
+  default: {
+    from: 'Equipe HeyDoctor <noreply@heydoctor.com>',
+  },
+};
