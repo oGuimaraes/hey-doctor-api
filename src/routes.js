d79ecd504bfcf1000a28338e357cc5be61415034
oGuimaraes <otaviovgsr@gmail.com>
2020-06-24 15:41:48 -0300

    feat: update notification as read

--- src/routes.js
+++ src/routes.js
@@ -33,6 +33,8 @@ routes.get('/appointments', AppointmentController.index);
 
 routes.get('/notifications', NotificationController.index);
 
+routes.put('/notifications/:id', NotificationController.update);
+
 routes.get('/schedule', ScheduleController.index);
 
 export default routes;
