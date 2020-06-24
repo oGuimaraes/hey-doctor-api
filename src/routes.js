82d61ba481ab9eddebb2da24b47604d503960109
oGuimaraes <otaviovgsr@gmail.com>
2020-06-24 16:10:26 -0300

    feat: cancellation appointment

--- src/routes.js
+++ src/routes.js
@@ -28,11 +28,10 @@ routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.post('/appointments', AppointmentController.store);
-
 routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/notifications', NotificationController.index);
-
 routes.put('/notifications/:id', NotificationController.update);
 
 routes.get('/schedule', ScheduleController.index);
