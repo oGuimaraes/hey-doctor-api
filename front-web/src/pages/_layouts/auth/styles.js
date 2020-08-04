b50b61ecc7b096410d500531725a88184183b443
oGuimaraes <otaviovgsr@gmail.com>
2020-08-04 09:41:46 -0300

    chore: install root import

--- front-web/src/pages/_layouts/auth/styles.js
+++ front-web/src/pages/_layouts/auth/styles.js
@@ -3,5 +3,4 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   height: 100%;
   background: linear-gradient(-120deg, #1b4965, #5fa8d3);
-  /*background: linear-gradient(-120deg, #1b4965, #5fa8d3);*/
 `;
